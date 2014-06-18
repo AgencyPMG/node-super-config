c42c85049655b1404479456cd7a69fed65e45340
Thales Lessa <thales.lessa@tcu.edu>
2014-06-18 14:12:53 -0500

    Added more tests

--- test/index.js
+++ test/index.js
@@ -26,4 +26,33 @@ describe('config/index', function() {
             assert.equal('test', config.get('notavariable', 'test'));
         });
     });
+
+    describe('#loadConfig', function() {
+        it('should tell user file does not exist', function() {
+            config.loadConfig('');
+        });
+
+        it('should load empty file', function() {
+            config.loadConfig([__dirname + '/config/emptyconfig']);
+        });
+
+        it('should load file', function() {
+            config.loadConfig([__dirname + '/config/config']);
+        });
+    });
+
+    describe('#connectDatabase', function() {
+        it('should call the function', function(done) {
+            config.connectDatabase(function() {
+                done();
+            });
+        });
+
+        it('should call the function with return', function(done) {
+            config.connectDatabase(function() {
+                done();
+                return done;
+            });
+        });
+    });
 });
